f60da522178a87f953a9df837286df532f4a3d6f
iammdtanvirrahman <mdtanvirrahman2022a@gmail.com>
2026-07-24 01:55:37 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,106 +1,155 @@
-const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const status = document.getElementById("status");
+const video=document.getElementById("video");
+const canvas=document.getElementById("canvas");
+const ctx=canvas.getContext("2d");
+const status=document.getElementById("status");
 
 
-canvas.width = 640;
-canvas.height = 480;
+canvas.width=640;
+canvas.height=480;
 
 
-// MediaPipe Hands
-const hands = new Hands({
 
-    locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-    }
+const hands=new Hands({
+
+locateFile:(file)=>{
+
+return "https://cdn.jsdelivr.net/npm/@mediapipe/hands/"+file;
+
+}
 
 });
+
 
 
 hands.setOptions({
 
-    maxNumHands: 2,
-    modelComplexity: 1,
-    minDetectionConfidence: 0.7,
-    minTrackingConfidence: 0.7
+maxNumHands:2,
 
-});
+modelComplexity:1,
 
+minDetectionConfidence:0.5,
 
-
-// Result পাওয়ার পর
-hands.onResults(results => {
-
-
-    ctx.clearRect(0,0,640,480);
-
-
-    ctx.drawImage(
-        results.image,
-        0,
-        0,
-        640,
-        480
-    );
-
-
-    if(results.multiHandLandmarks){
-
-
-        status.innerHTML =
-        "Hand Detected ✋";
-
-
-        for(const landmarks of results.multiHandLandmarks){
-
-
-            drawConnectors(
-                ctx,
-                landmarks,
-                HAND_CONNECTIONS,
-                {
-                    color:"#00ffcc",
-                    lineWidth:3
-                }
-            );
-
-
-            drawLandmarks(
-                ctx,
-                landmarks,
-                {
-                    color:"#ff0066",
-                    lineWidth:2
-                }
-            );
-
-
-        }
-
-
-    }
+minTrackingConfidence:0.5
 
 });
 
 
 
 
-// Camera
-const camera = new Camera(video, {
 
-    onFrame: async()=>{
+hands.onResults((results)=>{
 
-        await hands.send({
-            image:video
-        });
 
-    },
+ctx.clearRect(0,0,640,480);
 
-    width:640,
-    height:480
+
+
+ctx.drawImage(
+
+results.image,
+
+0,
+
+0,
+
+640,
+
+480
+
+);
+
+
+
+if(results.multiHandLandmarks && results.multiHandLandmarks.length>0){
+
+
+status.innerHTML="Hand Detected ✋";
+
+
+
+for(let hand of results.multiHandLandmarks){
+
+
+
+drawConnectors(
+
+ctx,
+
+hand,
+
+HAND_CONNECTIONS,
+
+{
+
+color:"#00ffcc",
+
+lineWidth:4
+
+}
+
+);
+
+
+
+drawLandmarks(
+
+ctx,
+
+hand,
+
+{
+
+color:"#ff0066",
+
+radius:5
+
+}
+
+);
+
+
+
+}
+
+
+
+}
+
+else{
+
+status.innerHTML="Show Hand ✋";
+
+}
+
+
 
 });
+
+
+
+
+
+
+const camera=new Camera(video,{
+
+onFrame:async()=>{
+
+await hands.send({
+
+image:video
+
+});
+
+},
+
+
+width:640,
+
+height:480
+
+
+});
+
 
 
 camera.start();
