790bb6c6ce9555054b9ccf0bc85360b8379b0e64
iammdtanvirrahman <mdtanvirrahman2022a@gmail.com>
2026-07-24 01:42:32 +0600

    Create app.js

---
+++ gesture-synth/app.js
@@ -0,0 +1,12 @@
+const video=document.getElementById("video");
+
+
+navigator.mediaDevices.getUserMedia({
+    video:true
+})
+.then(stream=>{
+    video.srcObject=stream;
+})
+.catch(error=>{
+    console.log(error);
+});
