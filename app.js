585397567a16a11e4e803a6670da4f4c208ecacc
iammdtanvirrahman <mdtanvirrahman2022a@gmail.com>
2026-07-24 01:53:11 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,106 @@
 const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 const status = document.getElementById("status");
 
 
-navigator.mediaDevices.getUserMedia({
-    video:{
-        width:640,
-        height:480
+canvas.width = 640;
+canvas.height = 480;
+
+
+// MediaPipe Hands
+const hands = new Hands({
+
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
     }
-})
-
-.then(stream=>{
-
-    video.srcObject = stream;
-
-    status.innerHTML="Camera Ready ✋";
-
-})
-
-.catch(error=>{
-
-    status.innerHTML="Camera Error ❌";
-
-    console.log(error);
 
 });
+
+
+hands.setOptions({
+
+    maxNumHands: 2,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.7,
+    minTrackingConfidence: 0.7
+
+});
+
+
+
+// Result পাওয়ার পর
+hands.onResults(results => {
+
+
+    ctx.clearRect(0,0,640,480);
+
+
+    ctx.drawImage(
+        results.image,
+        0,
+        0,
+        640,
+        480
+    );
+
+
+    if(results.multiHandLandmarks){
+
+
+        status.innerHTML =
+        "Hand Detected ✋";
+
+
+        for(const landmarks of results.multiHandLandmarks){
+
+
+            drawConnectors(
+                ctx,
+                landmarks,
+                HAND_CONNECTIONS,
+                {
+                    color:"#00ffcc",
+                    lineWidth:3
+                }
+            );
+
+
+            drawLandmarks(
+                ctx,
+                landmarks,
+                {
+                    color:"#ff0066",
+                    lineWidth:2
+                }
+            );
+
+
+        }
+
+
+    }
+
+});
+
+
+
+
+// Camera
+const camera = new Camera(video, {
+
+    onFrame: async()=>{
+
+        await hands.send({
+            image:video
+        });
+
+    },
+
+    width:640,
+    height:480
+
+});
+
+
+camera.start();
